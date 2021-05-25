0d858521f437bb40610bab02c670e6f1975c645c
baichuan72f <woshibaichuan@163.com>
2021-05-25 09:13:01 +0800

    V1.2

--- Assets/WWHY/RTSCamera/Scripts/RTSExt.cs
+++ Assets/WWHY/RTSCamera/Scripts/RTSExt.cs
@@ -1,265 +1,253 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using Cinemachine;
 using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.SocialPlatforms;
+using UnityEngine.EventSystems;
 
-[RequireComponent(typeof(CinemachineFreeLook)), RequireComponent(typeof(CinemachineCameraOffset))]
-public class RTSExt : CinemachineExtension
+namespace WWHY.RTSCamera
 {
-    CinemachineFreeLook _freeLook;
-    CinemachineCameraOffset _offset;
-    private bool _onDrag;
-    private Vector3 _startRoundPos;
-    private Vector3 _roundDis;
-    private Vector3 _startRoundView;
-    private Vector3 _startMovePos;
-    private Vector3 _moveDis;
-    private Vector3 _startMoveView;
-    private CinemachineFreeLook.Orbit[] _startOrbits;
-    public float moveSpeed = 100;
-    float keySpeed = 200;
-    float mouseSpeed = 1000;
-    public float rotaSpeed = 1.2f;
-    public float scaleSpeed = 10;
-    public float scaleMin = 0.2f;
-    public float scaleMax = 5;
-    float _scale = 1;
-
-    [Tooltip("The volume within which the camera is to be contained")]
-    public Collider m_BoundingVolume;
-
-    public bool cannotMoveAfterCollision = true;
-    int _clampState = 0;
-    int _nearFarState = 0;
-
-    protected override void OnEnable()
+    [RequireComponent(typeof(CinemachineFreeLook))]
+    public class RtsExt : CinemachineExtension
     {
-        base.OnEnable();
-        Init();
-    }
+        CinemachineFreeLook _freeLook; //基础相机
+        private Vector3 _startFollow; // 初始目标点     
+        private Vector3 _startRoundPos; //初始旋绕时相机位置
+        private Vector3 _startRoundInput; //初始旋绕时_freeLook组件的值
+        private Vector3 _startMovePos; //初始移动时相机位置
+        private Vector3 _moveDistance; //相机移动距离
+        private CinemachineFreeLook.Orbit[] _startOrbits; //_freeLook的三个Rig初始状态
 
-    private void Init()
-    {
-        _freeLook = GetComponent<CinemachineFreeLook>();
-        _offset = GetComponent<CinemachineCameraOffset>();
-        if (_freeLook != null)
+        public float keyMoveSpeed = 1f; //键盘移动系数
+        public float mouseMoveSpeed = 2f; //鼠标移动系数
+
+        float _scale = 1; //相机的操作比例（移动旋转缩放）
+        int _clampState = 0; //是否接触到边界
+        int _nearFarState = 0; //相机接近或远离范围中心
+        KeyCode[] _keyCodes;
+
+        private Vector3[] _disArr = new Vector3[]
+            {Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back};
+
+        public float moveSpeed = 0.05f; //移动速度
+        public float rotaSpeed = 1; //旋转速度
+        public float deltaSpeed = 1; //鼠标缩放系数
+
+        [Tooltip("Zhe range of Scale, x = minRange,y = maxRange")]
+        public Vector2 scaleRange = new Vector2(0.1f, 10);
+
+        [Tooltip("Disable On UI ?")] public bool disableOnUi = false;
+
+        [Tooltip("The volume within which the camera is to be contained")]
+        public Collider mBoundingVolume;
+
+        [Tooltip("after Collision is can not move ?")]
+        public bool cannotMoveAfterCollision = true;
+
+
+        KeyCode upKey = KeyCode.Q;
+        KeyCode downKey = KeyCode.E;
+        KeyCode leftKey = KeyCode.A;
+        KeyCode rightKey = KeyCode.D;
+        KeyCode forwardKey = KeyCode.W;
+        KeyCode backKey = KeyCode.S;
+
+        //初始化
+        void Start()
         {
-            _freeLook.m_XAxis.m_InputAxisName = String.Empty;
-            _freeLook.m_YAxis.m_InputAxisName = String.Empty;
-            _scale = 1;
-            _startOrbits = new CinemachineFreeLook.Orbit[_freeLook.m_Orbits.Length];
-            for (int i = 0; i < _freeLook.m_Orbits.Length; i++)
+            _freeLook = GetComponent<CinemachineFreeLook>();
+            if (_freeLook != null)
             {
-                _startOrbits[i] =
-                    new CinemachineFreeLook.Orbit(_freeLook.m_Orbits[i].m_Height, _freeLook.m_Orbits[i].m_Radius);
+                _freeLook.m_XAxis.m_InputAxisName = String.Empty;
+                _freeLook.m_YAxis.m_InputAxisName = String.Empty;
+                _scale = 1;
+                _startOrbits = new CinemachineFreeLook.Orbit[_freeLook.m_Orbits.Length];
+                for (int i = 0; i < _freeLook.m_Orbits.Length; i++)
+                {
+                    _startOrbits[i] =
+                        new CinemachineFreeLook.Orbit(_freeLook.m_Orbits[i].m_Height, _freeLook.m_Orbits[i].m_Radius);
+                }
+
+                _startFollow = _freeLook.Follow.position;
+                _keyCodes = new KeyCode[] {upKey, downKey, leftKey, rightKey, forwardKey, backKey};
+            }
+        }
+
+        protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam,
+            CinemachineCore.Stage stage,
+            ref CameraState state, float deltaTime)
+        {
+            if (_freeLook != null && _freeLook.Follow == null) return;
+            //储存开始值
+            if (Input.GetMouseButton(0) && Input.GetMouseButton(1))
+            {
+                _freeLook.Follow.position = Vector3.zero;
+                Reset();
+                return;
             }
 
-            Reset();
-        }
-    }
-
-    protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage,
-        ref CameraState state, float deltaTime)
-    {
-        _freeLook = vcam as CinemachineFreeLook;
-        if (!_freeLook) return;
-        if (!Input.GetMouseButton(1))
-        {
-            //滚轮缩放
-            if (Mathf.Abs(Input.mouseScrollDelta.y) > 0)
+            //在UI上失效
+            if (disableOnUi && EventSystem.current.IsPointerOverGameObject())
             {
-                scaleFreeLook(deltaTime);
+                Debug.Log(1);
+                return;
+            }
+
+            if (!Input.GetMouseButton(1))
+            {
+                //滚轮缩放
+                if (Mathf.Abs(Input.mouseScrollDelta.y) > 0)
+                {
+                    ScaleFreeLook(deltaTime);
+                }
+            }
+
+            //储存开始值
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartRound();
+            }
+
+
+            //执行视角围绕
+            if (Input.GetMouseButton(0))
+            {
+                var roundPos = (Input.mousePosition - _startRoundPos) * rotaSpeed;
+                RoundTarget(roundPos);
+            }
+
+            bool startKey = _keyCodes.Aggregate(false, (current, t) => current | Input.GetKeyDown(t));
+
+            //储存开始值
+            if (Input.GetMouseButtonDown(1) || startKey)
+            {
+                StartMove();
+            }
+
+            //相机移动输入
+            Vector3 moveInput = Vector3.zero;
+            for (var i = 0; i < _keyCodes.Length; i++)
+            {
+                if (Input.GetKey(_keyCodes[i])) moveInput += _disArr[i];
+            }
+
+            //如果有相机范围则限制移动
+            if (mBoundingVolume)
+            {
+                ColliderClamp(ref state);
+            }
+
+            bool moveKey = _keyCodes.Aggregate(false, (current, t) => current | Input.GetKey(t));
+
+            //执行平移
+            if (Input.GetMouseButton(1) || moveKey)
+            {
+                if (Mathf.Abs(Input.mouseScrollDelta.y) > 0)
+                {
+                    moveInput.z += Input.mouseScrollDelta.y * mouseMoveSpeed * deltaSpeed;
+                }
+
+                MoveXy(deltaTime, state, moveInput);
             }
         }
 
-        //储存开始值
-        if (Input.GetMouseButtonDown(0))
+
+        void ColliderClamp(ref CameraState state)
         {
+            var camPos = state.CorrectedPosition;
+            Vector3 displacement = mBoundingVolume.ClosestPoint(camPos) - camPos;
+            state.PositionCorrection += displacement;
+
+            //接触边界的状态
+            _clampState = displacement.magnitude.CompareTo(0);
+            if (!cannotMoveAfterCollision)
+            {
+                //若接触边界依然可以移动，则默认没有接触到边界
+                _clampState = 0;
+            }
+        }
+
+        private void StartRound()
+        {
+            _startRoundPos = Input.mousePosition;
+            _startRoundInput = new Vector2(_freeLook.m_XAxis.Value, _freeLook.m_YAxis.Value);
+        }
+
+        private void RoundTarget(Vector3 roundPos)
+        {
+            _freeLook.m_XAxis.Value = _startRoundInput.x + (roundPos.x * 360 / Screen.width);
+            _freeLook.m_YAxis.Value = _startRoundInput.y + (roundPos.y / Screen.height);
+        }
+
+        private void StartMove()
+        {
+            _startMovePos = Input.mousePosition;
+            _moveDistance = Vector3.zero;
+        }
+
+        private void MoveXy(float deltaTime, CameraState state, Vector3 keyMove)
+        {
+            //当前鼠标的移动距离
+            var mouseDis = (Input.mousePosition - _startMovePos) * -mouseMoveSpeed;
+            //将移动距离转化为相对于屏幕的移动比例
+            var mouseMove = new Vector3(mouseDis.x / Screen.width, mouseDis.y / Screen.height,
+                mouseDis.z / (Screen.width > Screen.height ? Screen.width : Screen.height));
+            var before = _freeLook.Follow.position;
+            //移动角度修正，确保每次移动鼠标距离相同时视野移动角度尽量一致
+            var dis = (_freeLook.VirtualCameraGameObject.transform.position - before).magnitude;
+            var after = before + state.FinalOrientation * (mouseMove + keyMove - _moveDistance) *
+                moveSpeed * dis * deltaTime;
+            var m = before.magnitude.CompareTo(after.magnitude);
+            if (m != 0) _nearFarState = m;
+            //Debug.Log("_clamp:" + _clampState + " _moveState:" + _nearFarState);
+            //如果相机没有接触到边界且正在远离
+            if (_clampState != 1 || _nearFarState != -1)
+            {
+                _freeLook.Follow.position = after;
+            }
+
+            _moveDistance = mouseMove;
+        }
+
+        private void Reset()
+        {
+            if (_freeLook != null)
+            {
+                _freeLook.Follow.position = _startFollow;
+            }
+
             StartRound();
-        }
-
-
-        //执行视角围绕
-        if (Input.GetMouseButton(0))
-        {
-            var roundPos = (Input.mousePosition + _roundDis - _startRoundPos) * rotaSpeed;
-            RoundTarget(roundPos);
-        }
-
-        bool startKey = Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.A) ||
-                        Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.E);
-
-        //储存开始值
-        if (Input.GetMouseButtonDown(1) || startKey)
-        {
             StartMove();
-        }
-
-        bool keyMove = false;
-        if (Input.GetKey(KeyCode.W))
-        {
-            _moveDis.z += deltaTime * keySpeed;
-            keyMove = true;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            _moveDis.z += -deltaTime * keySpeed;
-            keyMove = true;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            _moveDis.x += deltaTime * keySpeed;
-            keyMove = true;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            _moveDis.x += -deltaTime * keySpeed;
-            keyMove = true;
-        }
-
-        if (Input.GetKey(KeyCode.Q))
-        {
-            _moveDis.y += deltaTime * keySpeed;
-            keyMove = true;
-        }
-
-        if (Input.GetKey(KeyCode.E))
-        {
-            _moveDis.y += -deltaTime * keySpeed;
-            keyMove = true;
-        }
-
-        if (m_BoundingVolume)
-        {
-            ColliderClamp(vcam, stage, ref state, deltaTime);
-        }
-
-        //执行平移
-        if (Input.GetMouseButton(1) || keyMove)
-        {
-            //滚轮缩放
-            if (Mathf.Abs(Input.mouseScrollDelta.y) > 0)
+            for (int i = 0; i < _startOrbits.Length; i++)
             {
-                _moveDis.z += Input.mouseScrollDelta.y * deltaTime * mouseSpeed;
+                _freeLook.m_Orbits[i] =
+                    new CinemachineFreeLook.Orbit(_startOrbits[i].m_Height, _startOrbits[i].m_Radius);
             }
 
-            var moveDistance = (Input.mousePosition + _moveDis - _startMovePos) * moveSpeed;
-            MoveXY(ref state, moveDistance);
+            _scale = 1;
         }
 
-
-        //储存开始值
-        if (Input.GetMouseButton(0) && Input.GetMouseButton(1))
+        private void ScaleFreeLook(float deltaTime)
         {
-            Reset();
-        }
-    }
+            var add = Input.mouseScrollDelta.y * deltaTime * deltaSpeed;
+            if (Math.Abs(add) <= 0) return;
+            //合规判断
+            var s = _scale + add;
+            if (s <= 0) return;
 
-
-    void ColliderClamp(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage,
-        ref CameraState state, float deltaTime)
-    {
-        var camPos = state.CorrectedPosition;
-        Vector3 displacement = m_BoundingVolume.ClosestPoint(camPos) - camPos;
-        state.PositionCorrection += displacement;
-
-        //接触边界的状态
-        _clampState = displacement.magnitude.CompareTo(0);
-        if (!cannotMoveAfterCollision)
-        {
-            _clampState = 0;
-        }
-    }
-
-    private void StartRound()
-    {
-        _startRoundPos = Input.mousePosition;
-        _roundDis = Vector3.zero;
-        _startRoundView = new Vector2(_freeLook.m_XAxis.Value, _freeLook.m_YAxis.Value);
-    }
-
-    private void RoundTarget(Vector3 roundPos)
-    {
-        _freeLook.m_XAxis.Value = _startRoundView.x + (roundPos.x * 360 / Screen.width);
-        _freeLook.m_YAxis.Value = _startRoundView.y + (roundPos.y / Screen.height);
-    }
-
-    private void StartMove()
-    {
-        _startMovePos = Input.mousePosition;
-        _moveDis = Vector3.zero;
-        _startMoveView = _offset.m_Offset;
-        //_offsetZ = 0;
-    }
-
-    private void MoveXY(ref CameraState state, Vector3 moveDistance)
-    {
-        moveDistance = new Vector3(moveDistance.x / Screen.width, moveDistance.y / Screen.height,
-            moveDistance.z / (Screen.width > Screen.height ? Screen.width : Screen.height));
-        var before = _startMoveView;
-        var after = _startMoveView + moveDistance * _scale;
-        var m = before.magnitude.CompareTo(after.magnitude);
-        if (m != 0) _nearFarState = m;
-        //Debug.Log("_clamp:" + _clampState + " _moveState:" + _nearFarState);
-        if (_clampState != 1 || _nearFarState != -1)
-        {
-            _offset.m_Offset = _startMoveView + moveDistance * _scale;
-        }
-    }
-
-    private void Reset()
-    {
-        _startRoundPos = Input.mousePosition;
-        _startRoundView = new Vector2(_freeLook.m_XAxis.Value, _freeLook.m_YAxis.Value);
-        _roundDis = Vector3.zero;
-
-        _startMovePos = Input.mousePosition;
-        _startMoveView = Vector3.zero;
-        _moveDis = Vector3.zero;
-
-        _offset.m_Offset = Vector3.zero;
-    }
-
-    private void scaleFreeLook(float deltaTime)
-    {
-        var add = Input.mouseScrollDelta.y * deltaTime * scaleSpeed;
-        if (Math.Abs(add) <= 0) return;
-        //合规判断
-        var s = _scale + add;
-        if (s <= 0) return;
-        var targetScale = ScaleValue(s);
-        if (float.IsNaN(targetScale)) return;
-
-        // 是否可以缩小
-        bool canMin = scaleMax > _scale && s < _scale;
-        // 是否可以扩大
-        bool canMax = scaleMin < _scale && _scale > s;
-        bool canMove = (scaleMin < s && s < scaleMax) || canMin || canMax;
-        _scale = Mathf.Clamp(s, scaleMin, scaleMax);
-        if (canMove)
-        {
-            for (int i = 0; i < _freeLook.m_Orbits.Length; i++)
+            // 是否可以缩小
+            bool canMin = scaleRange.y > _scale && s < _scale;
+            // 是否可以扩大
+            bool canMax = scaleRange.x < _scale && _scale > s;
+            bool canMove = (scaleRange.x < s && s < scaleRange.y) || canMin || canMax;
+            _scale = Mathf.Clamp(s, scaleRange.x, scaleRange.y);
+            if (canMove)
             {
-                _freeLook.m_Orbits[i].m_Radius = _startOrbits[i].m_Radius * _scale;
-                _freeLook.m_Orbits[i].m_Height = _startOrbits[i].m_Height * _scale;
+                for (int i = 0; i < _freeLook.m_Orbits.Length; i++)
+                {
+                    _freeLook.m_Orbits[i].m_Radius = _startOrbits[i].m_Radius * _scale;
+                    _freeLook.m_Orbits[i].m_Height = _startOrbits[i].m_Height * _scale;
+                }
             }
         }
     }
-
-    float ScaleValue(float before)
-    {
-        return Mathf.Pow(before, 0.5f);
-    }
-
-    class VcamExtraState
-    {
-        public Vector3 m_previousDisplacement;
-        public float confinerDisplacement;
-    };
 }
